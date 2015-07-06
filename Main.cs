1df4eb944a3dfcc18a3179f84872267bc7b61f19
ThaiSmartDev <dev@thaismartdev.com>
2015-07-06 13:01:54 +0700

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerPOS_Online
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=powerddtest;AccountKey=6b+dBUr/E6uC1l+jVFT4ZgXgQSXGJwrqlF1UR3NFTcpyScMLZgCGV9C2612oZB+/DvVH0R1QrhUdooiLprgSxQ==");
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("people");
+            table.CreateIfNotExists();
+        }
+    }
+}
